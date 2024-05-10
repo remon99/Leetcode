6369191892bf44fcc4eb8e534c266d798973fd54
remon <110497620+remon99@users.noreply.github.com>
2024-05-10 21:35:23 +0600

    Create kth smallest prime fraction.py

---
+++ kth smallest prime fraction.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        n=len(arr)
+        pq=[]
+
+        for i in range(n-1):
+            heapq.heappush(pq,(arr[i]/arr[-1],i,n-1))
+
+        while k-1:
+            _,i,j=heapq.heappop(pq)
+            if i < j-1:
+                heapq.heappush(pq,(arr[i]/arr[j-1],i,j-1))
+            k-=1
+        return arr[pq[0][1]],arr[pq[0][2]]
